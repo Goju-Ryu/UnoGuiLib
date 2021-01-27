b81eb5d988d7928058a788152671e8074934d284
Goju Ryu <mads1606@gmail.com>
2021-01-27 20:58:47 +0100

    Implement simple input buttons

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,32 @@
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.unit.IntSize
+import ui.Buttons
 
-fun main() = Window {
+fun main() = Window(
+    title = "My Uno Game",
+    size = IntSize(1280, 1024)
+) {
+    App()
+}
+
+@Composable
+fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            Button(onClick = {
+                text = "Hello, Desktop!"
+            }) {
+                Text(text)
+            }
+
+            Buttons("Hello", "Hi", "Hola") {name -> println(name) }
         }
     }
 }
---
+++ src/main/kotlin/ui/BasicUserInput.kt
@@ -0,0 +1,33 @@
+package ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+/**
+ * This composable creates an arbitrary number of buttons.
+ * The buttens will have a standard behavior that should depend only on the supplied name
+ */
+@Composable
+internal fun Buttons(vararg buttonNames: String, buttonOnClick: ButtonOnClick = {}) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        for (name in buttonNames) {
+            Button(onClick = { buttonOnClick(name) }, Modifier.padding(5.dp)) {
+                Text(name)
+            }
+        }
+    }
+}
+
+typealias ButtonOnClick = (name: String) -> Unit
