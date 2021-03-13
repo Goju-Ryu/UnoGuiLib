ae03ca359dfb9722599a14359220795859b86b65
Goju Ryu <mads1606@gmail.com>
2021-03-13 15:06:42 +0100

    Update compose and kotlin version

--- src/main/kotlin/ui/BasicUserInput.kt
+++ src/main/kotlin/ui/BasicUserInput.kt
@@ -1,22 +1,17 @@
 package ui
 
-import androidx.compose.desktop.DesktopMaterialTheme
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.runtime.*
-import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import kotlin.properties.Delegates
 
 /**
  * This composable creates an arbitrary number of buttons.
